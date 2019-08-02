f7e08b6b364b38b33be21d5998ce60b56c4927b5
Vitus Lehner <>
2019-08-02 16:12:59 +0200

    Implement storage readers

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -22,12 +22,44 @@ class DataStorage {
         fs.writeFileSync(filePath, JSON.stringify(station));
     }
 
+    public readEeaStation(countryCode: string, stationId: string): Station[] | null {
+        const countryDir = `${this.storageDir}/${countryCode}`;
+        const stationDir = `${countryDir}/${stationId}`;
+        if (!fs.existsSync(stationDir)) {
+            return null;
+        }
+        const files = fs.readdirSync(stationDir, { withFileTypes: true });
+        const fileContents = [];
+
+        for (const f of files) {
+            if (!f.isFile()) {
+                continue;
+            }
+            const filePath = `${stationDir}/${f.name}`;
+            let content = JSON.parse(fs.readFileSync(filePath, "utf8"));
+            if (f.name.endsWith("_location.json")) {
+                content = { location: content };
+            }
+            fileContents.push(content);
+        }
+        return fileContents;
+    }
+
     public saveEeaLocationIndex(index: EeaLocationIndexEntry[]) {
         const indexDir = `${this.storageDir}/_index`;
         this.createDirectoryIfNotExists(indexDir);
 
         const filePath = `${indexDir}/eeaLocationIndex.json`;
         fs.writeFileSync(filePath, JSON.stringify(index));
+    }
+
+    public readEeaLocationIndex(): EeaLocationIndexEntry[] | null {
+        const indexDir = `${this.storageDir}/_index`;
+        const filePath = `${indexDir}/eeaLocationIndex.json`;
+        if (!fs.existsSync(filePath)) {
+            return null;
+        }
+        return JSON.parse(fs.readFileSync(filePath, "utf8"));
     }
 
     public saveEeaStationLocation(countryCode: string, stationId: string, location: Location) {
