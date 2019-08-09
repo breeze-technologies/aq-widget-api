f5987fb967c9e2920c56da23f04b0dd2033aec6a
Vitus Lehner <>
2019-08-09 18:19:45 +0200

    Allow cross-origin API access

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use("/", indexRouter);
 
 export default app;
