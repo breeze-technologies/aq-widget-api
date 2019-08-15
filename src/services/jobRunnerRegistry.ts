3456ffb5b3555edf01f094ba9375a780db21bf5d
Vitus Lehner <>
2019-08-15 12:28:06 +0200

    Minor promise enhancement

--- src/services/jobRunnerRegistry.ts
+++ src/services/jobRunnerRegistry.ts
@@ -2,8 +2,9 @@ import { EeaUtdFetcherConfig } from "aq-client-eea/dist/models/eeaUtdFetcherConf
 import { addressRetrieverJob } from "./jobs/addressRetrieverJob";
 import { eeaFetchJob } from "./jobs/eeaFetchJob";
 
-export function eeaFetchRunner(fetchConfig: EeaUtdFetcherConfig, callback: (result: any, error?: any) => void) {
-    eeaFetchJob(fetchConfig).then((locationIndex) => callback(locationIndex));
+export async function eeaFetchRunner(fetchConfig: EeaUtdFetcherConfig, callback: (result: any, error?: any) => void) {
+    const locationIndex = await eeaFetchJob(fetchConfig);
+    callback(locationIndex);
 }
 
 export async function addressRetrieverRunner(input: string, callback: () => void) {
