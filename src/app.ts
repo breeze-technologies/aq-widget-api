ec3c9cd76760b91d8461105ca29dc9c47e12cae8
Vitus Lehner <>
2019-09-10 15:03:20 +0200

    Make CORS header configurable

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 // import path from "path";
 
+import { CORS_ALLOW_ORIGIN } from "./config";
 import indexRouter from "./routes/index";
 
 const app = express();
@@ -10,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", CORS_ALLOW_ORIGIN);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
-export const STORAGE_DIR = "./datastore";
+export const STORAGE_DIR = process.env.STORAGE_DIR || "./datastore";
+export const CORS_ALLOW_ORIGIN = process.env.CORS_ALLOW_ORIGIN || "*";
 export const MAX_STATION_DISTANCE = 5;
 export const MAX_MEASUREMENT_AGE_HOURS = 6;
 export const MIN_AQI_MEASUREMENT_COUNT = 1;
